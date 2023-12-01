028dd13e9309d23916d524d830ebb771774017d6
phacntt <trananhpha2001@gmail.com>
2023-12-01 17:33:20 +0700

    add eslint + prettier + css folder + custom input

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,21 @@
-import InputComponent from "./components/input-component";
-
+import InputComponent from './components/input-component'
+import './App.css'
 function App() {
   return (
-    <div
-      className="App"
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <div
-        style={{
-          border: "2px solid black",
-          padding: "20px",
-          marginTop: "20px",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          flexDirection: "column",
-          width: "250px",
-        }}
-      >
+    <div className="App">
+      <div className="card">
         <div>
           <img
-            style={{ width: "250px", height: "250px" }}
+            className="thumbnail"
             src="https://www.w3schools.com/html/img_girl.jpg"
-            alt={`images`}
+            alt={'images'}
           />
         </div>
-        <div
-          style={{
-            margin: "10px 0 10px 0",
-            fontSize: "20px",
-            fontWeight: "bold",
-          }}
-        >
-          Áo khoác mùa đông ấm áp
-        </div>
+        <div className="title-card">Áo khoác mùa đông ấm áp</div>
         <InputComponent />
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/input-component.tsx
+++ src/components/input-component.tsx
@@ -1,63 +1,50 @@
-import { ChangeEvent, useState } from "react";
+import { type ChangeEvent, useState } from 'react'
+import '../css/input-component.css'
 
 const InputComponent: React.FC = () => {
-  const [price, setprice] = useState(0);
-  const [isFocus, setIsFocus] = useState<boolean>(false);
+  const [price, setprice] = useState('')
+  const [isFocus, setIsFocus] = useState<boolean>(false)
 
   const handleFocus = () => {
-    setIsFocus(!isFocus);
-  };
+    setIsFocus(!isFocus)
+  }
 
   const handleOnChangeInput = (e: ChangeEvent<HTMLInputElement>) => {
-    setprice(Number(e.target.value));
-  };
+    const numericValue = e.target.value.replace(/[^0-9.]/g, '')
+    const formattedValue = parseFloat(numericValue).toLocaleString('en-US', {
+      maximumFractionDigits: 3,
+    })
+    setprice(formattedValue)
+  }
 
-  const handleOnBlur = (e: React.FocusEvent<HTMLInputElement>) => {
-    setIsFocus(!isFocus);
-  };
+  const handleOnBlur = () => {
+    setIsFocus(!isFocus)
+  }
 
   return (
     <>
       {isFocus ? (
-        <div
-          className="InputComponent"
-          onBlur={handleOnBlur}
-          style={{ marginTop: "5px" }}
-        >
+        <div className="InputComponent" onBlur={handleOnBlur}>
+          <div className="icon-input-border">
+            <div className="icon-input">Price</div>
+          </div>
           <input
             autoFocus
-            style={{
-              fontWeight: "bold",
-              fontFamily: "Time New Romane",
-              fontSize: "20px",
-              width: "230px",
-              padding: "8.5px 10px 8.5px",
-            }}
+            className="field-input"
             type="number"
-            placeholder="price"
+            placeholder="Fill Price"
             defaultValue={price}
             onChange={handleOnChangeInput}
             onBlur={handleOnBlur}
           />
         </div>
       ) : (
-        <div
-          style={{
-            marginTop: "5px",
-            fontWeight: "bold",
-            fontSize: "20px",
-            width: "230px",
-            padding: "10px",
-            border: "2px solid black",
-            borderRadius: "3px",
-          }}
-          onClick={handleFocus}
-        >
+        <div className="field-price" onClick={handleFocus}>
           $ {price}
         </div>
       )}
     </>
-  );
-};
+  )
+}
 
-export default InputComponent;
+export default InputComponent
